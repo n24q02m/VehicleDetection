f46e42a64b806ef216d8344fb33f953bf4e615fc
n24q02m <quangminh2422004@gmail.com>
2024-11-04 08:28:41 +0700

    feat: update training configuration and model references for yolov8m-ghost-p2

--- src/training/train.py
+++ src/training/train.py
@@ -5,10 +5,10 @@ assert amp_allclose(YOLO("yolo11n.pt"), im)
     ```
     thành:
     ```
-assert amp_allclose(YOLO("yolov8s-ghost.yaml"), im)
+assert amp_allclose(YOLO("yolov8m-ghost-p2.yaml"), im)
     ```
     
-    - Chỉnh sửa dòng 6 file /envs/vehicle-detection/Lib/site-packages/ultralytics/cfg/models/v8/yolov8-ghost.yaml từ:
+    - Chỉnh sửa dòng 6 file /envs/vehicle-detection/Lib/site-packages/ultralytics/cfg/models/v8/yolov8-ghost-p2.yaml từ:
     ```
 nc: 80
     ```
@@ -23,12 +23,12 @@ import shutil
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model_name = "yolov8s-ghost.yaml"
+    model_name = "yolov8m-ghost-p2.yaml"
     model_dir = "./models"
     model_save_dir = f"{model_dir}/{model_name}"
     data_dir = "./data/soict-hackathon-2024_dataset"
     train_project = "./runs"
-    train_name = "train-yolov8s-ghost-edit"
+    train_name = "train-yolov8m-ghost-p2"
 
     if os.path.exists(model_save_dir):
         shutil.move(model_save_dir, model_name)
@@ -37,22 +37,23 @@ if __name__ == "__main__":
 
     model.train(
         data=f"{data_dir}/data.yaml",
-        epochs=20,
-        batch=16,
-        lr0=1e-2,
+        fraction=1.0,  # 0.1 là sử dụng 10% dữ liệu
+        cache="disk",  # True nếu cache ram
+        epochs=100,  # 300 cho train from scratch, 100 cho finetune
+        time=10,
+        batch=-1, # -1 là tự động chọn batch size
+        patience=10, # 30 cho train from scratch, 10 cho finetune
+        lr0=1e-3,  # 1e-3 đối với finetune
         device=0,
         project=train_project,
         name=train_name,
         exist_ok=True,
-        pretrained=False,
-        optimizer="SGD",
-        momentum=0.937,
-        weight_decay=5e-4,
+        pretrained=False,  # True nếu finetune
+        optimizer="SGD", # AdamW với finetune
+        weight_decay=5e-4,  # 1e-5 đối với finetune
         warmup_epochs=5,
-        warmup_momentum=0.8,
-        warmup_bias_lr=0.1,
-        amp=True,
-        patience=10,
+        augment=True,
+        multi_scale=True,
         cos_lr=True,
         plots=True,
         show=True,
