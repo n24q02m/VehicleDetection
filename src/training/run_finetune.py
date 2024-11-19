717a92450ab4a0c6704f5b0129c1274e7728c1f9
n24q02m <quangminh2422004@gmail.com>
2024-11-19 10:39:33 +0700

    refactor: Add finetuning and training scripts with environment checks and dataset setup

---
+++ src/training/run_finetune.py
@@ -0,0 +1,26 @@
+import os
+
+# Cài đặt các thư viện cần thiết
+os.system("pip install -U ultralytics kaggle tqdm albumentations")
+
+# Kiểm tra môi trường và thiết lập thư mục dataset
+if "KAGGLE_URL_BASE" in os.environ:
+    # Trên Kaggle
+    os.system("git clone https://github.com/n24q02m/VehicleDetection.git")
+    os.chdir("VehicleDetection")
+    dataset_dir = "/kaggle/working/VehicleDetection/data/soict-hackathon-2024_dataset"
+else:
+    # Trên máy ảo
+    dataset_dir = "C:\\Users\\Administrator\\Desktop\\VehicleDetection\\src\\data\\soict-hackathon-2024_dataset"
+
+# Set YOLO dataset directory
+os.system(f"yolo settings datasets_dir={dataset_dir}")
+
+# Chạy hàm main từ finetune.py
+from src.training.finetune import main as finetune_main
+
+finetune_main()
+
+from src.utils.gitaction import git_push_changes
+
+git_push_changes("refactor: Update after run")
